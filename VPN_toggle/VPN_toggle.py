484e3e9f69bb95b6673f87c6168edbcd69285de3
John Ellison <L-E-son@users.noreply.github.com>
2016-05-08 16:42:26 -0400

    Update VPN_toggle.py

--- VPN_toggle/VPN_toggle.py
+++ VPN_toggle/VPN_toggle.py
@@ -5,7 +5,7 @@ routerIP = '192.168.1.1'
 VPNpage = 'http://' + routerIP + '/Advanced_OpenVPNClient_Content.asp' #ASUS merlin firmware ONLY
 
 #you must first download the ChromeDriver from https://sites.google.com/a/chromium.org/chromedriver/home and specify it in the parameters below like so: Chrome("l:/ocation/of/driver/")
-browser = webdriver.Chrome("ADD YOUR CHROMEDRIVER LOCATION HERE") #You can use whichever browser you want, just be sure to make sure you have the correct driver mentioned above (look at the documentation or change the browser on this line and execute, you will be given the link)
+browser = webdriver.Chrome("ADD YOUR CHROMEDRIVER LOCATION HERE") #You can use whichever browser you want, just be sure to make sure you have the correct driver mentioned above (look at the documentation or change the browser on this line and execute, you will be given the link by the debugger)
 browser.get(VPNpage)
 
 userLogin = browser.find_element_by_name('login_username')
